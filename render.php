5b7282a74f41743991940d867cf8eead920275f3
gerardobort <gerardobort@gmail.com>
2013-04-18 23:32:18 -0300

    some changes

--- render.php
+++ render.php
@@ -1,3 +1,21 @@
 <?php
+ 
+$fullname = urldecode($_GET['fullname']);
 
+//Carregar imagem
+$rImg = ImageCreateFromJPEG("artwork/v1.jpg");
+ 
+//Definir cor
+$cor = imagecolorallocate($rImg, 0, 0, 0);
 
+//$font = 'arial.ttf';
+//imagettftext($im, 20, 0, 11, 21, $grey, $font, $text);
+
+//Escrever nome
+imagestring($rImg, 5, 126, 22, $fullname, $cor);
+ 
+//Header e output
+header('Content-type: image/jpeg');
+imagejpeg($rImg, NULL, 100);
+ 
+?
