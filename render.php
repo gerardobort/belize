18ad2dd6be6bbcb62c70085994227c97cc249061
gerardobort <gerardobort@gmail.com>
2013-04-19 00:17:16 -0300

    adding date fields

--- render.php
+++ render.php
@@ -1,22 +1,18 @@
 <?php
-//phpinfo();die;
  
 $fullname = urldecode($_GET['fullname']);
-
-//Carregar imagem
 $rImg = imagecreatefromjpeg("artwork/v1.jpg");
- 
-//Definir cor
 $color = imagecolorallocate($rImg, 0, 0, 0);
 
-$font = 'artwork/fonts/badhand.ttf';
 $font = 'artwork/fonts/sign.ttf';
-imagettftext($rImg, 20, 0, 260, 405, $color, $font, $fullname);
+$font2 = 'artwork/fonts/badhand.ttf';
 
-//Escrever nome
-//imagestring($rImg, 5, 260, 390, $fullname, $color);
- 
-//Header e output
+imagettftext($rImg, 20, 0, 260, 405, $color, $font2, $fullname);
+//imagettftext($rImg, 20, 0, 280, 505, $color, $font, date('dd', time()));
+
+imagettftext($rImg, 20, 0, 320, 458, $color, $font2, date('d', time()));
+imagettftext($rImg, 20, 0, 480, 458, $color, $font2, date('F', time()));
+imagettftext($rImg, 20, 0, 700, 458, $color, $font2, date('y', time()));
+
 header('Content-type: image/jpeg');
 imagejpeg($rImg, NULL, 100);
- 
