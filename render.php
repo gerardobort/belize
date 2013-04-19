c3ccb1b8b3628e954dffc8105b37b5a765e7f2ab
gerardobort <gerardobort@gmail.com>
2013-04-19 00:23:44 -0300

    improving artwork

--- render.php
+++ render.php
@@ -2,17 +2,16 @@
  
 $fullname = urldecode($_GET['fullname']);
 $rImg = imagecreatefromjpeg("artwork/v1.jpg");
-$color = imagecolorallocate($rImg, 0, 0, 0);
+$color = imagecolorallocate($rImg, 63, 71, 73);
 
 $font = 'artwork/fonts/sign.ttf';
-$font2 = 'artwork/fonts/badhand.ttf';
+$font1 = 'artwork/fonts/badhand.ttf';
+$font2 = 'artwork/fonts/celine.ttf';
 
-imagettftext($rImg, 20, 0, 260, 405, $color, $font2, $fullname);
-//imagettftext($rImg, 20, 0, 280, 505, $color, $font, date('dd', time()));
-
-imagettftext($rImg, 20, 0, 320, 458, $color, $font2, date('d', time()));
-imagettftext($rImg, 20, 0, 480, 458, $color, $font2, date('F', time()));
-imagettftext($rImg, 20, 0, 700, 458, $color, $font2, date('y', time()));
+imagettftext($rImg, 24, 0, 260, 405, $color, $font2, $fullname);
+imagettftext($rImg, 22, 0, 320, 458, $color, $font2, date('d', time()));
+imagettftext($rImg, 22, 0, 480, 458, $color, $font2, date('F', time()));
+imagettftext($rImg, 22, 0, 700, 458, $color, $font2, date('y', time()));
 
 header('Content-type: image/jpeg');
 imagejpeg($rImg, NULL, 100);
