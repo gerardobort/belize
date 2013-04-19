d2272673ae3ab4ab89c0296061d5ff09b6a9d216
gerardobort <gerardobort@gmail.com>
2013-04-19 00:30:48 -0300

    general tweaks

--- render.php
+++ render.php
@@ -1,6 +1,6 @@
 <?php
  
-$fullname = urldecode($_GET['fullname']);
+$fullname = substr(urldecode($_GET['fullname']), 0, 17);
 $rImg = imagecreatefromjpeg("artwork/v1.jpg");
 $color = imagecolorallocate($rImg, 63, 71, 73);
 
@@ -8,10 +8,10 @@ $font = 'artwork/fonts/sign.ttf';
 $font1 = 'artwork/fonts/badhand.ttf';
 $font2 = 'artwork/fonts/celine.ttf';
 
-imagettftext($rImg, 24, 0, 260, 405, $color, $font2, $fullname);
-imagettftext($rImg, 22, 0, 320, 458, $color, $font2, date('dS', time()));
-imagettftext($rImg, 22, 0, 480, 458, $color, $font2, date('F', time()));
-imagettftext($rImg, 22, 0, 700, 458, $color, $font2, date('y', time()));
+imagettftext($rImg, 28, 0, 260, 407, $color, $font2, $fullname);
+imagettftext($rImg, 24, 0, 320, 458, $color, $font2, date('dS', time()));
+imagettftext($rImg, 24, 0, 470, 458, $color, $font2, date('F', time()));
+imagettftext($rImg, 24, 0, 700, 458, $color, $font2, date('y', time()));
 
 header('Content-type: image/jpeg');
 imagejpeg($rImg, NULL, 100);
